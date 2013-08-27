df24fc37ad9f45f268ec9097bbab02e3ff0af66f
dkokate@gmail.com <dkokate@gmail.com>
2013-08-27 15:15:34 +0800

    Finish refactor

--- spec/requests/secret_sauce_pages_spec.rb
+++ spec/requests/secret_sauce_pages_spec.rb
@@ -3,33 +3,75 @@ require 'spec_helper'
 describe "Secret Sauce pages" do
   subject {page}
   
+  shared_examples_for "all Secret Sauce static pages" do
+    it {should have_selector('h1', text: heading)} 
+    it {should have_title(full_title(page_title))}
+    # -------------- Test for links to common pages ------------------ #
+    it "should have the right links on the layout" do
+      click_link "Home"
+      expect(page).to have_title(full_title(''))
+
+      click_link "Help"
+      expect(page).to have_title(full_title('Help'))
+
+      # click_link "Sign In"
+      # expect(page).to have_title(full_title('Sign In'))
+
+      click_link "About"
+      expect(page).to have_title(full_title('About Us'))
+
+      click_link "Contact"
+      expect(page).to have_title(full_title('Contact'))
+
+      # click_link "News"
+      # expect(page).to have_title(full_title('About Us'))
+
+      # click_link "Secret Sauce"
+      # expect(page).to have_title(full_title('Secret Sauce'))
+    end
+  end
+  
   describe "Home page" do
     before { visit root_path}
+    let(:heading) {'Secret Sauce'}
+    let(:page_title) {''}
     
-    it {should have_content('Secret Sauce')}
-    it {should have_title(full_title('')) }
-      # save_and_open_page  
-    it {should_not have_title("| Home")}  
+    it_should_behave_like "all Secret Sauce static pages"
+    it {should_not have_title("| Home")} 
+    # click_link "Sign up now!"
+    # expect(page).to
+    
+    # it {should have_content('Secret Sauce')} # it {should have_title(full_title('')) } # save_and_open_page 
   end
   
   describe "Help page" do
     before {visit help_path}
+    let(:heading) {'Help'}
+    let(:page_title) {'Help'}
     
-    it {should have_content('Help')}
-    it {should have_title("Secret Sauce | Help")}
+    it_should_behave_like "all Secret Sauce static pages"
+    
+    # should have_selector('h1', text: 'Help')} # it {should have_title("Secret Sauce | Help")}
   end
   
   describe "About Us page" do
     before {visit about_path}
+    let(:heading) {'About Us'}
+    let(:page_title) {'About Us'}
     
-    it {should have_content('About Us')}
-    it {should have_title(full_title("About Us"))}
+    it_should_behave_like "all Secret Sauce static pages"
+    
+    # it {should have_selector('h1', text: 'CAbout Us')} # it {should have_title(full_title("About Us"))}
   end
   
   describe "Contact page" do
     before {visit contact_path}
+    let(:heading) {'Contact'}
+    let(:page_title) {'Contact'}
     
-    it {should have_content('Contact')} 
-    it {should have_title(full_title("Contact"))}
+    it_should_behave_like "all Secret Sauce static pages"
+    
+    # it {should have_selector('h1', text: 'Contact')} # it {should have_title(full_title("Contact"))}
   end
+  
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,13 @@
-def full_title(page_title)
-  base_title = "Secret Sauce"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+
+include ApplicationHelper
+
+# With above include, full_title helper in Utilities is redundant 
+# full_title of ApplicationHelper has been tested
+# def full_title(page_title)
+#   base_title = "Secret Sauce"
+#   if page_title.empty?
+#     base_title
+#   else
+#     "#{base_title} | #{page_title}"
+#   end
+# end
