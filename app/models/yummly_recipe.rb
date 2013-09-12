cec7c9fddab9ca452f4b2ad3a4ae46d6f30deb0f
dkokate@gmail.com <dkokate@gmail.com>
2013-09-12 13:11:42 +0800

    Removed yum_recipe

--- app/models/yummly_recipe.rb
+++
@@ -1,4 +0,0 @@
-class YummlyRecipe < ActiveResource::Base
-  self.site = "http://api.yummly.com/v1/api/recipes?_app_id=16f1c757&_app_key=30223f5d3e7f54a438aeb94a7f94d85e"
-  # "http://16f1c757:30223f5d3e7f54a438aeb94a7f94d85e@api.yummly.com/v1"
-end
